698d54a1c3182f5667fbbcfd0704142f841a6e44
arunkarthi4008215 <85730631+arunkarthi4008215@users.noreply.github.com>
2023-06-14 15:33:52 +0530

    Update StudentController.cs

--- JWTRestApi/Controllers/StudentController.cs
+++ JWTRestApi/Controllers/StudentController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Authorization;
 
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
+// Arun Kumar
 namespace JWTRestApi.Controllers
 {
     [Route("api/[controller]")]
